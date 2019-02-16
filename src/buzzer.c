fa552482e27e338affb93e922752fc009088f39b
wjykk <no@no>
2019-02-15 23:28:58 -0700

    Miserable stub at starting the buzzer PWM

--- src/buzzer.c
+++ src/buzzer.c
@@ -2,7 +2,7 @@
 #include "segment.h"
 
 #define ALARM_SWITCHED_ON (BUTTON_LAT & SWT_ALARM)
-static bool alarm_ringing;
+#define BUZZER_RINGING (BUZZER_LAT & BUZZER_PIN)
 
 inline void buzzer_trigger()
 {
@@ -11,14 +11,26 @@ inline void buzzer_trigger()
             && minutes_tens.value == minutes_tens.alarm_value
             && minutes_ones.value == minutes_ones.alarm_value)
     {
-        // TODO setup PWM
-        alarm_ringing = true;
+        // TODO setup PWM using these steps (from section 29.1.9 in the manual):
+        // 1. Set buzzer pin to input
+        BUZZER_TRIS |= BUZZER_PIN;
+        
+        // 2. Configure PWM polarity
+        // Skip, it will always be active high
+        
+        // 3. Load PWM period value
+        // TODO
+        
+        // 4. Load PWM duty cycle
+        // TODO
+        
+        // oh my fuck this is getting out of hand
     }
 }
 
 inline void buzzer_tick()
 {
-    if (alarm_ringing)
+    if (BUZZER_RINGING)
     {
         if (ALARM_SWITCHED_ON)
         {
@@ -27,7 +39,6 @@ inline void buzzer_tick()
         else
         {
             // TODO de-setup PWM
-            alarm_ringing = false;
         }
     }
 }
