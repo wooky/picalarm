726acd3410b1c649a548dfaa1cc76cfd191acee5
wjykk <no@no>
2020-07-19 19:52:19 -0600

    Implement IDE simulation mode

--- src/main.c
+++ src/main.c
@@ -4,21 +4,28 @@
 #include "button.h"
 #include "buzzer.h"
 
-void main()
-{
-    init();
-    // Spin, waiting for an interrupt
-    while (1)
-    {
-        CLRWDT();
-    }
-}
-
+#ifdef __DEBUG
+void on_interrupt()
+#else
 void __interrupt() on_interrupt()
+#endif
 {
     PIR1bits.CCP1IF = 0;
     clock_tick();
     button_tick();
     segment_render();
     buzzer_tick();
+}
+
+void main()
+{
+    init();
+    // Spin, waiting for an interrupt
+    while (1)
+    {
+#ifdef __DEBUG
+        on_interrupt();
+#endif
+        CLRWDT();
+    }
 }
